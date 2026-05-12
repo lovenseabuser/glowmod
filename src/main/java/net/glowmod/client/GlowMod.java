45802566b7742b6ce626e38da25381887952e00e
lovenseabuser <botikden@gmail.com>
2026-05-13 02:17:43 +0300

    Update GlowMod.java

--- src/main/java/net/glowmod/client/GlowMod.java
+++ src/main/java/net/glowmod/client/GlowMod.java
@@ -1,9 +1,8 @@
 package net.glowmod.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.MinecraftClient;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.entity.player.PlayerEntity;
@@ -13,7 +12,7 @@ import org.lwjgl.glfw.GLFW;
 public class GlowMod implements ClientModInitializer {
 
     public static boolean glowEnabled = false;
-    private static KeyBinding keyBinding;
+    public static KeyBinding keyBinding;
     private static boolean wasPressed = false;
 
     @Override
@@ -37,11 +36,6 @@ public class GlowMod implements ClientModInitializer {
                 );
             }
             wasPressed = isPressed;
-
-            for (PlayerEntity player : client.world.getPlayers()) {
-                if (player == client.player) continue;
-                player.setGlowing(glowEnabled);
-            }
         });
     }
 }
